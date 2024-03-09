3e38b941d650d624fec1adcca1cbef1fb2732316
samgeosa <samuel.sasaki@student.president.ac.id>
2024-03-09 10:12:53 +0700

    fifth commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */ 
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  }
 
-export default nextConfig;
+module.exports = nextConfig
